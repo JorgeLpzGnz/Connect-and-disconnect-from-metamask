104aa3d7900ad9fdb09f15aec41a7f466a086351
jorge <geolg763@gmail.com>
2022-05-02 15:41:11 -0500

    add change network buttom

--- index.js
+++ index.js
@@ -1,27 +1,67 @@
-const connect = document.getElementById('connect')
-const disconnect = document.getElementById('disconnect')
+const connect = document.getElementById('connect');
+const disconnect = document.getElementById('disconnect');
+const change = document.getElementById('change')
+const address = '';
+const abi = {}
 
 connect.addEventListener('click', () => login())
 disconnect.addEventListener('click', () => logout())
+change.addEventListener('click', () => changeNw())
 
 console.log(window.userWalletAddress)
 
 async function login(){
 
-  let accounts;
+  if(window.ethereum){
+    let flag = false
 
-  await window.ethereum
-              .request({ method: 'wallet_requestPermissions',
-                         params: [
-                              {
-                             eth_accounts: {}
-                              }
-                            ]
-                      })
-              .then(accounts = await window.ethereum
-                                           .request({ method: 'eth_requestAccounts' })
-                                           )
-  window.userWalletAddress = accounts[0]
+    await window.ethereum
+                .request({ method: 'wallet_requestPermissions',
+                           params: [
+                                {
+                               eth_accounts: {}
+                                }
+                              ]
+                        })
+                .then(flag = true)
+
+    if(flag){
+
+    const accounts = await window.ethereum
+                           .request({ method: 'eth_requestAccounts' })
+    ethereum.on('connect', info => console.log(`connect to${info}`))
+    ethereum.on('disconnect', err => console.log(`connect to${err}`))
+    window.userWalletAddress = accounts[0]
+    
+    // const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    // const signer = provider.getSigner();
+
+    // const contract = new ethers.Contract(address, abi, signer);
+
+    // contract.on("Count", (method, count, caller) => {
+    //   console.log("Event occured from smart contract: ", method, count, caller);
+
+ // });
+  }
+  }else{
+    console.log('install metamask')
+  }
+
+
+}
+
+async function changeNw(){
+  await window.ethereum.request({
+    "id": 1,
+    "jsonrpc": "2.0",
+    "method": "wallet_switchEthereumChain",
+    "params": [
+      {
+        "chainId": "0x1",
+      }
+    ]
+  })
 }
 
 function logout(){
